9546dbbe1eb3e647d3e27ae1c078045e403ad82f
dummy-ie <leocariomarcus@gmail.com>
2023-10-24 08:14:05 +0800

    Added Internal Dice singleton class Added Orientation Lock singleton class Renamed "Dice" to "External Dice" Added ResultExternal() method to External Dice

--- Assets/Scripts/Dice/ExternalDice.cs
+++ Assets/Scripts/Dice/ExternalDice.cs
@@ -4,30 +4,40 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class Dice : MonoBehaviour
+public class ExternalDice : MonoBehaviour
 {
     [SerializeField]
     private Light _diceLight;
 
-    [SerializeField][Range(0.1f, 60f)]
+    [SerializeField]
+    [Range(0.1f, 60f)]
     private float _timeOut = 10f;
 
-    [SerializeField][Range(0.1f, 1000f)]
+    [SerializeField]
+    [Range(0.1f, 1000f)]
     private float _rollForce = 10f;
 
-    [SerializeField][Range(0.1f, 1000f)]
+    [SerializeField]
+    [Range(0.1f, 1000f)]
     private float _torqueAmount = 10f;
-    private Rigidbody _rigidbody;
-    private List<GameObject> _dieFaces = new(20);
-    private bool rolling = false;
 
     [SerializeField]
     private AccelerometerProperty _accelerometerProperty;
 
+    private Rigidbody _rigidbody;
+    private List<GameObject> _dieFaces = new(20);
+    private bool _rolling = false;
+
+    private int _result = 0;
+    public int Result { get { return _result; } }
+    
+    // private int _difficultyClass;
+    // public int DifficultyClass { get { return _difficultyClass; } }
+
     private void CheckAccelerometer()
     {
         if (Math.Abs(Input.acceleration.x) >= _accelerometerProperty.MinChangeX
-        && !rolling)
+        && !_rolling)
         {
             FireAccelerometerEvent();
         }
@@ -37,18 +47,18 @@ public class Dice : MonoBehaviour
     {
         Vector3 deltaTransform = Vector3.zero;
         deltaTransform.x = Input.acceleration.x * (_accelerometerProperty.SpeedX * Time.deltaTime);
-        RollDice(Input.acceleration * (_accelerometerProperty.SpeedX * Time.deltaTime));
+        YeetDice(Input.acceleration * (_accelerometerProperty.SpeedX * Time.deltaTime));
         // transform.Translate(deltaTransform);
     }
 
-    public void RollDice(Vector2 direction)
+    private void YeetDice(Vector2 direction)
     {
         StopAllCoroutines();
 
         _diceLight.enabled = false;
 
         _rigidbody.constraints = RigidbodyConstraints.None;
-        rolling = true;
+        _rolling = true;
         _rigidbody.AddForce(_rollForce * Time.deltaTime * direction, ForceMode.Impulse);
         _rigidbody.AddTorque(_torqueAmount * Time.deltaTime * direction, ForceMode.Impulse);
         StartCoroutine(WaitForDieToStop());
@@ -60,10 +70,11 @@ public class Dice : MonoBehaviour
 
         // wait until die settles or if the time runs out
         while (!_rigidbody.IsSleeping() && Time.time < timeOut) yield return null;
- 
+
         yield return null; // wait another frame
 
         int dieValue = GetDieValue();
+        _result = dieValue;
         Debug.Log("Player rolled: " + dieValue);
 
         // OnDieRolled?.Invoke(dieValue); // Event for later
@@ -76,12 +87,20 @@ public class Dice : MonoBehaviour
         if (faceUp != null)
             int.TryParse(faceUp.name, out number);
 
-        rolling = false;
+        _rolling = false;
 
         _diceLight.enabled = true;
 
 
         return number;
+    }
+
+    public bool ResultExternal(int difficultyClass, int modifier = 0)
+    {
+        if (_result + modifier >= difficultyClass)
+            return true;
+
+        return false;
     }
 
     // public void OnSwipe(SwipeEventArgs args)
@@ -95,7 +114,7 @@ public class Dice : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();    
+        _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.constraints = RigidbodyConstraints.FreezePosition;
 
         foreach (Transform child in transform)
