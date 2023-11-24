587e269cb0bacf7cad69315ee221859fe819c1d9
dummy-ie <leocariomarcus@gmail.com>
2023-11-24 09:28:48 +0800

    Added move on tap for combat entities

--- Assets/Scripts/Camera/FocusCameraOnTap.cs
+++ Assets/Scripts/Camera/FocusCameraOnTap.cs
@@ -1,46 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using UnityEditor;
 using UnityEngine;
 
 public class FocusCameraOnTap : MonoBehaviour, ITappable
 {
-    public CinemachineVirtualCamera _cam;
+    [SerializeField]
+    private CinemachineVirtualCamera _cam;
 
     public void OnTap(TapEventArgs args)
     {
         if (args.HitObject != gameObject)
             Debug.LogWarning(name + " Receiving tap from different HitObject.");
 
-        // Debug.Log(name + " OnTap()");
-        // if (lastObject != null)
-        //     Debug.Log(lastObject.name + " OnTap()");
 
-        // activate our highlight
-        AnimatedHighlight current = GetComponentInChildren<AnimatedHighlight>(true);
-        if (current != null)
-            current.Play();
-        else
-            Debug.LogWarning("OnTap(): " + name + "'s current AnimatedHighlight couldn't be found.", this);
-        
         // deactivate current active vcam
         GameObject currentCam = Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject;
 
-        GameObject lastObject = currentCam.transform.parent.gameObject;
+        // Debug.Log(currentCam + " OnTap()");
+        // Debug.Log(_cam.gameObject.name + " OnTap()");
+        
         currentCam.SetActive(false);
         _cam.gameObject.SetActive(true);
-
-        // deactivate last highlight
-        AnimatedHighlight last;
-        if (lastObject != null && lastObject != gameObject)
-            last = lastObject.GetComponentInChildren<AnimatedHighlight>(true);
-        else
-            return;
-
-        if (last != null)
-            last.Pause();
-        else
-            Debug.LogWarning("OnTap(): " + lastObject.name + "'s current AnimatedHighlight couldn't be found.", this);
 
         // causes abrupt cuts
         // _cam.Follow = transform;
