c07518e457fec51a82b46460d6ac361b04436ebf
Arkaenaz <flooper2234@gmail.com>
2023-11-30 23:18:09 +0800

    fixed other billboards

--- Assets/Scripts/Gestures/GestureReceiver.cs
+++ Assets/Scripts/Gestures/GestureReceiver.cs
@@ -35,19 +35,36 @@ public abstract class GestureReceiver : MonoBehaviour
             Debug.LogError("Couldn't subscribe to any gestures. GestureManager is null!");
             return;
         }
-
         if (_gestures.HasFlag(ReceiveGestures.Tap))
+        {
+            Debug.Log("GestureManager subscribed to Tap.");
             GestureManager.Instance.OnTap += OnTap;
+        }
         if (_gestures.HasFlag(ReceiveGestures.Drag))
+        {
+            Debug.Log("GestureManager subscribed to Drag.");
             GestureManager.Instance.OnDrag += OnDrag;
+        }
         if (_gestures.HasFlag(ReceiveGestures.Swipe))
+        {
+            Debug.Log("GestureManager subscribed to Swipe.");
             GestureManager.Instance.OnSwipe += OnSwipe;
+        }
         if (_gestures.HasFlag(ReceiveGestures.Pan))
+        {
+            Debug.Log("GestureManager subscribed to Pan.");
             GestureManager.Instance.OnPan += OnPan;
+        }
         if (_gestures.HasFlag(ReceiveGestures.Spread))
+        {
+            Debug.Log("GestureManager subscribed to Spread.");
             GestureManager.Instance.OnSpread += OnSpread;
+        }
         if (_gestures.HasFlag(ReceiveGestures.Rotate))
+        {
+            Debug.Log("GestureManager subscribed to Rotate.");
             GestureManager.Instance.OnRotate += OnRotate;
+        }
     }
 
     protected void UnsubscribeToGestures()
