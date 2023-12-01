3300cdabcfb25a4730646e040320ab22ee43a550
dummy-ie <leocariomarcus@gmail.com>
2023-12-01 23:03:13 +0800

    Added party manager spawn position offset

--- Assets/Scripts/PartyManager.cs
+++ Assets/Scripts/PartyManager.cs
@@ -9,9 +9,17 @@ public class PartyManager : Singleton<PartyManager>
 
     public void SpawnPartyMembers(Transform _spawn)
     {
+        Vector3[] spawnOffsets = {Vector3.forward, Vector3.right, Vector3.left, Vector3.back};
+        int i = 0;
+        int offset = 5;
         foreach (GameObject partyMember in _partyMembers)
         {
-            Instantiate(partyMember, _spawn.position, _spawn.rotation);
+            Instantiate(partyMember, _spawn.position + (offset * spawnOffsets[i]), _spawn.rotation);
+
+            if (i + 1 > spawnOffsets.Length)
+                i = 0;
+            else
+                i++;
         }
     }
 }
